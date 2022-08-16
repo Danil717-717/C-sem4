750a528505d18e62177beac2840e28340fc55ff7
Danil <danila_monster@mail,ru>
2022-08-16 18:06:12 +0300

    Задача третья

---
+++ Task003/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N
+// 4 -> 24 
+// 5 -> 120
+
+int multiPlic(int a)
+{
+    int result = 1;
+    for(int i = 1; i <= a; i++)
+    {
+        result*=i;
+    }
+    return result;
+}
+
+//Console.WriteLine(multiPlic(4));
+
+void PrintMult(int a)
+{
+    Console.WriteLine($"Произведение числа {a} равно {multiPlic(a)}");
+}
+
+PrintMult(4);
+PrintMult(5);
+PrintMult(6);
