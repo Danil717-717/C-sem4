c0d259ad01f3e61768d6c30e515cbe103c8386ae
Danil <danila_monster@mail,ru>
2022-08-16 16:42:11 +0300

    Задача первая

---
+++ Task001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А
+
+int Sum(int a)
+{
+    int result = 0;
+    for (int number = 0; number <= a; number++)
+    {
+        result += number;
+    }
+    return result;
+}
+
+void PrintSum(int a)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {a} равна {Sum(a)}");
+}
+
+PrintSum(5);
+PrintSum(10);
+PrintSum(0);
+PrintSum(-20);
+
+//Console.WriteLine(Sum(5)); // 1+2+3+4+5 = 15
+//Console.WriteLine(Sum(10));
+//Console.WriteLine(Sum(0));
+//Console.WriteLine(Sum(-20));
