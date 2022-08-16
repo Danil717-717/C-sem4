590da83a920c8a7c72d48ea537d990a810380b7a
Danil <danila_monster@mail,ru>
2022-08-16 19:54:45 +0300

    Задача четвертая верс2

---
+++ Task004_1/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] ArrayRnd(int n)
+{
+    int[] arr = new int [n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write(" [ ");
+    for ( int i = 0; i < arr.Length; i++)
+    {
+        if (i>0) Console.Write(", ");
+        Console.Write($"{arr[i]}");
+    }
+        Console.WriteLine("]");
+}
+
+PrintArray(ArrayRnd(8));
+PrintArray(ArrayRnd(8));
+PrintArray(ArrayRnd(8));
+PrintArray(ArrayRnd(8));
